6166dce14307507c6ebdc1aee44d1e1d83743527
Geyayu <2335061113@st.usst.edu.cn>
2026-01-06 16:41:10 +0800

    增添视频网页并进行相应拓展与细化

---
+++ video-site/src/main/java/com/advertising/video/VideoSiteApplication.java
@@ -0,0 +1,11 @@
+package com.advertising.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoSiteApplication.class, args);
+    }
+}
